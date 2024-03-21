3c1a60dd8e5bdcb668d4442aef91b968d851e37a
Ognjen <ognjen.devops030@gmail.com>
2024-03-21 13:28:52 +0100

    int final

--- CS50 python int.py
+++ CS50 python int.py
@@ -1,4 +1,4 @@
-# INT
+# INT , celi brojevi
 # kalkulator
 
 x = input("Prvi broj:")
@@ -8,3 +8,26 @@ y = input("Drugi broj:")
 z = int(x) + int(y)
 
 print(z)
+
+#you can nest functions like this
+
+i = int(input("treci broj:"))
+n = int(input("cetvrti broj:"))
+
+
+# can be done like this also
+
+print(i + n)
+
+# FLOAT , decimalni brojevi
+
+i = float(input("decimale broj:"))
+n = float(input("decimale broj:"))
+
+# round , zaokruzi brojeve , , 2 znaci da zaokruzi na 2 decimale, ne mora se koristio opciono je
+
+z = round(i + n, 2)
+print(z)
+
+# radi isto sto i z = round(i + n, 2) pomocu f stringa
+print(f"{z:.2f}")
