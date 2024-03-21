bdc05f0d411b93b759aaffd462da0d3f3cb51440
Ognjen <ognjen.devops030@gmail.com>
2024-03-21 17:59:23 +0100

    functions final

--- CS50 functions.py
+++ CS50 functions.py
@@ -1,8 +1,14 @@
 # DEF , pravi funkciju koju sami kreiramo i definisemo
 
+def main():
+    name = input("Ime ? ")
+    # ovde smo pozvali hello funkciju i ova izvrsava kod koji je dole napisan u okviru hello funkcije
+    hello(name) 
+
+# ovde je hello funkcija 
 def hello(to):
-    print("hello", to)
+    # ovde se nalazi kod koji se izvrsava u okviru hello funkcije
+    print("hello", to) 
 
-name = input("Ime ? ")
-
-hello(name)
+# da bi ostale funkcije radile na kraju programa moramo pozvati main funkciju
+main()
