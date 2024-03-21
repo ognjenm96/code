5277f480a67ed4c97fe44a8910b755edd7118d07
Ognjen <ognjen.devops030@gmail.com>
2024-03-21 08:46:03 +0100

    cs50 python

--- 3.py
+++
@@ -1,3 +0,0 @@
-width = 15
-height = 12.0
-print(height/3)
---
+++ CS50 python.py
@@ -0,0 +1,5 @@
+# = je operator dodele == znaci jednako za pogram
+# using user input, by default input is expecting string input
+name = input("Your name ? ") 
+# printing user input
+print("Hello, ", name) 
