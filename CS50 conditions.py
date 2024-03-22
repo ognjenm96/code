53c1996ebf4d1db2f123293f0dae3b233e5d2753
Ognjen <ognjen.devops030@gmail.com>
2024-03-22 13:50:58 +0100

    if else

--- CS50 conditions.py
+++ CS50 conditions.py
@@ -7,3 +7,31 @@ elif x > y:
     print("x je vece od y")
 else:
     print("x je jednako y")
+
+if x < y or x > y: # if x != y postize isti rezultat kao sa or
+    print("x nije jednako y")
+else:
+    print("x je jednako y")
+
+broj_poena = int(input("Moj broj peana: "))
+
+if broj_poena >= 90 and broj_poena <= 100:
+    print("Ocena je 5!")
+elif broj_poena >= 80 and broj_poena <=90:
+    print("Ocena je 4!")
+else:
+    print("Ocena je manja od 4")
+
+x =  int(input("Unesi broj: "))
+
+if x % 2 == 0:
+    print("paran broj")
+else:
+    print("neparan broj")
+
+# funkcija za odradjevanje pranog i nepranog broja
+def paran(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
