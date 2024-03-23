4f741056fc2a0e8f28eeb2ff884db6d0138ac75f
Ognjen <ognjen.devops030@gmail.com>
2024-03-23 15:35:48 +0100

    push

--- CS50 loops.py
+++ CS50 loops.py
@@ -1,6 +1,12 @@
 # this is going to be an infinite loop
-
+# zato sto je i=3 a uslove je da i nije 0, zbog toga sto se nemenja
+# vrednost i loop ce se vreteti besknacno
 # i = 3
 # while i !=0:
 #    print("av!")
 
+# ovako je tacna petlja koja ce uraditi print 3 puta iz ovog primera
+i = 3
+while i !=0:
+    print("AV")
+    i = i - 1
