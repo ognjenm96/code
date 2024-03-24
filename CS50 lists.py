caae96a695ed9f5ea2440c2dd6e2fd44260757a3
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 20:57:16 +0100

    pip and pipx

--- CS50 lists.py
+++ CS50 lists.py
@@ -11,3 +11,6 @@ for ucenici in ucenici:
 for i in range(len(ucenici)): # prvi deo for petlje u ovom slucaju i je varijabla koju kreiramo u toj petlji
     print(ucenici[i])
     print (i + 1, ucenici[i]) # 
+
+# variable_name[1:] na ovaj nacin mozemo da odredimo koje elemente cemo uzeti iz neke liste
+# ovde je 1 drugi element a ako nemamo nista nakon : onda ce ispisati sve nakon drugog
---
+++ packages.py
@@ -0,0 +1,3 @@
+# pypi.org ovde mozemo skinuti dodatne pakete za pajton
+# python ima svoj paket meneager pip
+#  python has requests librery for API usage can be installed with pip
