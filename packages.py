a89e7ef0710fb48897e892382a2464a5a8c15fe5
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 21:39:01 +0100

    APIs

--- packages.py
+++ packages.py
@@ -1,3 +1,17 @@
 # pypi.org ovde mozemo skinuti dodatne pakete za pajton
 # python ima svoj paket meneager pip
-#  python has requests librery for API usage can be installed with pip
+# python has requests librery for API usage can be installed with pip
+
+# USING API request to get a song information form itunes
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+    
+i = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
+n = i.json()
+# loop da izlista 10 imana pesama zadatok benda od strane korisnika
+for final in n["results"]:
+    print(final["trackName"])
