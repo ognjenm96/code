e224450d65e9599f71477c144bab6181114063ae
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 16:17:57 +0100

    mario final

--- mario.py
+++ mario.py
@@ -1,3 +1,10 @@
-print("#")
-print("#")
-print("#")
+def main():
+    print_stub(4) # ovde dajemo funkciji ranger koliko puta ce ispisati #
+   
+def print_stub(kocka):
+    for i in range(kocka):
+        for n in range(kocka):
+            print("#" , end=" ")
+        print()
+    
+main()
