72c8d6867ce50a82efc0d61551dcaf4349cfc4d6
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 16:51:55 +0100

    error

--- execeptions.py
+++ execeptions.py
@@ -1,7 +1,7 @@
 # try se koristi da se proba kod ispod njega
 try:
-    x = input(")
+    x = input(") # ovde postoji SyntaxError
     print()
 # except se koristi da odradi nesto ako nesto krene po zlu da ispise to
 except SyntaxError:
-    print(SystemError)
+    print(SystemError) # ovde ispise koji je SynaxError
