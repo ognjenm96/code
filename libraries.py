cbf08351731ca34613e972e850e519f12782bb31
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 18:50:57 +0100

    sys

--- libraries.py
+++ libraries.py
@@ -10,3 +10,12 @@ print(dinar)
 novcic = random.choice(["glava", "pismo"]) # random je ovde modul a choice jedana od njegovih funkcija
 print(novcic)
 
+# randint daje randombroj u okviru koji mu postaimo ovde, 1 do 10
+broj = random.randint(1, 10)
+print(broj)
+
+# ova funkcija mesa raspored iz lisste i printa ih
+karte = ["kralj", "kraljica", "dzoker", "as"]
+random.shuffle(karte)
+for karta in karte:
+    print(karta)
---
+++ sys.py
@@ -0,0 +1,7 @@
+import sys
+
+print("zdravo, moje ime je:", sys.argv[1])
+
+# ognjenm@mns-ognjenm:~/code$ python3 sys.py Ogi 
+# ovo se pojavljuje jer je Ogi prvi arg koji smo uneli u cmd
+# zdravo, moje ime je: Ogi
