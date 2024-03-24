bab433db1f6bc36e5d341b63d3cce5ef719a79f0
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-24 22:41:34 +0100

    more chages

--- test_kalkulatora.py
+++ test_kalkulatora.py
@@ -2,6 +2,7 @@ from kalkulator import kvadrat
 
 def main():
     test_kvadrat()
+    test_int()
 
 def test_kvadrat():
     if kvadrat(2) != 4:
@@ -13,6 +14,9 @@ def test_kvadrat():
       assert kvadrat(4) == 16
     except AssertionError:
         print("kvadrat od 4 nije 16")
+
+def test_int():
+    assert kvadrat(6) == 36 # ovaj deo nije dobar zajedno za def funkcijom iznad
 
 if __name__ == "__main__":
     main()
