b34727db9a27209ada5fa85a93b7bcfa6e4c3452
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-27 00:05:05 +0100

    regex1

--- validate.py
+++ validate.py
@@ -3,7 +3,12 @@ import re # import the regular expression module
 email = input("Enter your email: ").strip()
 
 # Check if the email is valid
-if re.search("..*@.*", email):
+# ^ means start of the string
+# .+ means one or more of any character
+# \w means any word character (a-z, A-Z, 0-9, _)
+# \d means any digit (0-9)
+# \s means any whitespace 
+if re.search(r"^\w+@\w+\.\w", email): # The email must end with .rs
     print("Valid email")
 else:
     print("Invalid email")
