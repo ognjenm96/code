1ea24bdc76b5a64de4f24063c551fc697191fcbb
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-27 01:20:59 +0100

    new forma\ting

--- formating12.py
+++ formating12.py
@@ -1,3 +1,9 @@
+import re
+
 url = input("Unesi URL: ").strip()
 
-print(f"URL: {url}")
+# re.sub - zamjenjuje dio stringa koji odgovara regularnom izrazu
+
+username = re.sub(r"https://", "", url)
+
+print(f"Korisničko ime: {username}")
