eee0aea7d7bdad704c85582b02c1a2331f51f223
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-27 01:41:38 +0100

    regex final

--- formating12.py
+++ formating12.py
@@ -3,7 +3,8 @@ import re
 url = input("Unesi URL: ").strip()
 
 # re.sub - zamjenjuje dio stringa koji odgovara regularnom izrazu
-
-username = re.sub(r"https://", "", url)
+# re.split - dijeli string na dijelove koji odgovaraju regularnom izrazu
+# ^ - označava početak stringa
+username = re.sub(r"^(https?://)?(www\.)?/(.+)", "", url)
 
 print(f"Korisničko ime: {username}")
