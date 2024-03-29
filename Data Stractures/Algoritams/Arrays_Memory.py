d532eb30fb0efb8dabb7d02ceeec63cde0998d87
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-29 12:51:16 +0100

    arrays

---
+++ Data Stractures/Algoritams/Arrays_Memory.py
@@ -0,0 +1,23 @@
+# What is an array ?
+# An array is a data structure that stores a fixed-size sequence of elements of the same type.
+# Here's an example of creating a simple array in Python:
+
+# in Python an array is a list but it's not the case in oter programing langueges
+
+my_array = [1, 2, 3, 4, 5]
+print(my_array)
+
+# Example 1: Array of strings
+my_array = ["apple", "banana", "cherry"]
+print(my_array)
+
+# Example 2: Array of booleans
+my_array = [True, False, True, True]
+print(my_array)
+
+# Example 3: Array of mixed data types
+my_array = [1, "apple", True, 3.14]
+print(my_array[1]) # 1 in here reprezents 1st value in array in the casee apple
+
+# this kind of arrays are specific for python as it can store mixed data types in a list
+# other programing langes don't have this flexability
