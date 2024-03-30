f091cef53b2f7d93d6aea06e1e87e3918140ceae
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-30 13:23:02 +0100

    adding index.html

--- flask/flask1.py
+++ flask/flask1.py
@@ -1,18 +1,21 @@
-from flask import Flask, redirect, url_for
+# Flask is a web framework for Python. It is used to create web applications.
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # Create a Flask app
 
-@app.route("/")
-def home():
-    return "Hello! Main page <h1>HELLO<h1>"
+@app.route("/") # Home page 
+def home(name):
+    # render_template() function is used to render a template.
+    return render_template("index.html")
 
-@app.route("/<name>")
+@app.route("/<name>") # app.route() decorator to tell Flask what URL should trigger the function
 def user(name):
     return f"Hello {name}!"
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("user", name="Admin"))
+    # redirect() function is used to redirect the user to another endpoint or URL.
+    return redirect(url_for("user", name="Admin")) # Redirect to the user() function
 
 if __name__ == "__main__":
     app.run()
