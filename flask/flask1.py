1613c3d376253d0382e40fd083051bc439f59b34
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-30 13:57:43 +0100

    fix the bug

--- flask/flask1.py
+++ flask/flask1.py
@@ -5,23 +5,7 @@ app = Flask(__name__) # Create a Flask app
 
 @app.route("/<name>") # Home page 
 def home(name):
-    # render_template() function is used to render a template.
-    return render_template("index.html")
-
-
-
-
-
-
-
-@app.route("/<name>") # app.route() decorator to tell Flask what URL should trigger the function
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    # redirect() function is used to redirect the user to another endpoint or URL.
-    return redirect(url_for("user", name="Admin")) # Redirect to the user() function
+    return render_template("index.html", content = name, r=2)
 
 if __name__ == "__main__":
     app.run()
