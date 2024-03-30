84e3f43112962034c9fbabfc0dc20f7a23e93a79
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-30 14:30:48 +0100

    adding some bootstrap

--- flask/flask1.py
+++ flask/flask1.py
@@ -5,7 +5,12 @@ app = Flask(__name__) # Create a Flask app
 
 @app.route("/<name>") # Home page 
 def home(name):
-    return render_template("index.html", content = name, r=2)
+    # render_template() function is used to render a template.
+    # It takes the name of the HTML file as an argument.
+    # The HTML file should be in a templates folder in the same directory as the script.
+    # The content variable is passed to the HTML file.
+    # The content variable is used to display the name on the web page.
+    return render_template("index.html", content = name)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) # Run the Flask app, debug=True will print out possible Python errors on the web page
