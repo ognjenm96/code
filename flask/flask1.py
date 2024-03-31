3bfce57dbf7594c0c770e706f88a1fec3ca26a24
Ognjen <ognjen.mitic.mes@gmail.com>
2024-03-31 10:48:30 +0200

    adding sessio functunality

--- flask/flask1.py
+++ flask/flask1.py
@@ -1,27 +1,39 @@
 # Flask is a web framework for Python. It is used to create web applications.
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__) # Create a Flask app
+app.secret_key = "comples" # Secret key for session
+app.permanent_session_lifetime = timedelta(minutes=30) # Set the session lifetime to 30 minutes
 
-@app.route("/") # Home page 
-def home():
-    # render_template() function is used to render a template.
-    # It takes the name of the HTML file as an argument.
-    # The HTML file should be in a templates folder in the same directory as the script.
-    # The content variable is passed to the HTML file.
-    # The content variable is used to display the name on the web page.
+@app.route("/") 
+def home(): # Home page 
     return render_template("index.html")
 
 @app.route("/login", methods=["POST", "GET"])
-def login():
+def login(): # Login page
     if request.method == "POST":
-        r
+        session.permanent = True # Set the session to permanent
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user")) # Redirect to the user page
         return render_template("login.html")
 
-@app.route("/user/<usr>")
-def user(usr):
-    return render_template("")
+@app.route("/user")
+def user(): # User page
+    if "user" in session:
+        user = session["user"] # Get the user from the session
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout(): # Logout page
+    session.pop("user", None) # Remove the user from the session 
+    return redirect(url_for("login")) # Redirect to the login page
 
 if __name__ == "__main__":
     app.run(debug=True) # Run the Flask app, debug=True will print out possible Python errors on the web page
