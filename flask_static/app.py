dfe1ae5a0aafb4bdb9d16cb718f76d7eb90dd963
Ognjen <ognjen.mitic.mes@gmail.com>
2024-04-03 13:41:41 +0200

    ..

--- flask_static/app.py
+++ flask_static/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_static.admin.admin_app import admin_app
+import os
 
 app = Flask(__name__) # Create a Flask app
 app.register_blueprint(admin_app, url_prefix="/admin") # Register the blueprint
