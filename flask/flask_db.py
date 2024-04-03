86b134563303130b46f6c90308df4825bec43213
Ognjen <ognjen.mitic.mes@gmail.com>
2024-04-03 10:21:29 +0200

    adding sql schema and function to init the db to db.py

--- flask/flask_db.py
+++ flask/flask_db.py
@@ -2,7 +2,7 @@ import sqlite3
 import click
 from flask import current_app, g
 
-def get_b():
+def get_db():
     if 'flask_db' not in g:
         g.flask_db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -16,3 +16,16 @@ def close_flask_db(e=None):
 
     if flask_db is not None:
         flask_db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
