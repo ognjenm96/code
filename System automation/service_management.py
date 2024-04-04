9391c72149eb6f827e8abcb9ab560fd2baf41562
Ognjen <ognjen.mitic.mes@gmail.com>
2024-04-04 15:02:50 +0200

    wip sys utils

--- System automation/service_management.py
+++ System automation/service_management.py
@@ -1,6 +1,7 @@
 # This script is used to check the disk usage, find big files, check CPU usage and check memory usage of a remote server.
 import psutil
 import paramiko
+from psutil._common import bytes2human
 
 def check_disk_usage():
     ssh = paramiko.SSHClient()
@@ -17,10 +18,10 @@ def find_big_files():
     print(stdout.read().decode())
 
 def check_cpu_usage():
-    print(psutil.cpu_percent(2))
+    print(psutil.cpu_percent(interval=1, percpu=True))
 
 def check_memory_usage():
-    print(psutil.virtual_memory().percent)
+    print(psutil.virtual_memory().free / (1024 * 1024) + psutil.virtual_memory().used / (1024 * 1024))
 
 check_disk_usage()
 find_big_files()
