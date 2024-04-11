9441ab1dca8291d2fb64346438eb9b1b11d01590
Ognjen <ognjen.mitic.mes@gmail.com>
2024-04-11 23:25:05 +0200

    djano project1

--- django_dev/webdev/notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpath = [
-    path("index.html" , views.index, name="index")
-]
--- django_dev/webdev/notes/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request): # This is the view function
-    return render(request, 'notes/index.html') # This is the view template
